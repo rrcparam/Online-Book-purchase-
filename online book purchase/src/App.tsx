08f782f57ac3a3df0ed353fa996bd2c6954234d5
Paramdeep Singh <psingh20@rrc.ca>
2026-02-02 19:39:31 -0600

    created homepage and add header navigation

--- online book purchase/src/App.tsx
+++ online book purchase/src/App.tsx
@@ -1,14 +1,17 @@
 
-import BookList from "./Components/book_catalog.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-   <>
-      <Header />
-      <BookList />
-     
-    </>
+    <BrowserRouter>
+      <Header wishlistCount={0} />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- online book purchase/src/Components/Header.tsx
+++ online book purchase/src/Components/Header.tsx
@@ -1,16 +1,21 @@
-const Header = () => {
+import { Link } from "react-router-dom";
+
+type HeaderProps = {
+  wishlistCount: number;
+};
+
+export default function Header({ wishlistCount }: HeaderProps) {
   return (
     <header>
       <h1>Online Book Purchaser</h1>
 
       <nav className="nav-links">
-        <button>Home</button>
-        <button>Search</button>
-        <button>Wishlist</button>
-        <button>Shop</button>
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
+        <Link to="/wishlist">
+          Wishlist ({wishlistCount})
+        </Link>
       </nav>
     </header>
   );
-};
-
-export default Header;
+}
